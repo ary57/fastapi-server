390635d9ae4e29dae177b9212a59784674561186
abhishek aryal <abhishek.ary7@gmail.com>
2026-03-18 19:23:50 -0500

    initial commit

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI in Docker with uv!"}
-
-@app.get("/add/{a}/{b}")
-def add_numbers(a: int, b: int):
-    return {"result": a + b}
